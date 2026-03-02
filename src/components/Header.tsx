140a65b63c19e51908a1072d4d8d9c29fb319fb6
santi <santi@re-envision.org>
2026-03-02 22:13:58 +0000

    add more items to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,17 +22,47 @@ export default function Header() {
         </Link>
 
         <nav className="header__nav" aria-label="Main navigation">
-          <ul className="header__nav-list">
-            <li>
-              <Link href="/" className="header__nav-link">Home</Link>
-            </li>
-            <li>
-              <Link href="/about" className="header__nav-link">About</Link>
-            </li>
-            <li>
-              <Link href="/contact" className="header__nav-link">Contact</Link>
-            </li>
-          </ul>
+        <ul className="header__nav-list">
+  <li>
+    <Link href="/" className="header__nav-link">Home</Link>
+  </li>
+
+  {/* About Dropdown */}
+  <li className="header__nav-item dropdown">
+    <button className="header__nav-link dropdown-toggle">About</button>
+    <ul className="dropdown-menu">
+      <li><Link href="/about/who-we-are" className="dropdown-item">Who We Are</Link></li>
+      <li><Link href="/about/team" className="dropdown-item">Our Team</Link></li>
+      <li><Link href="/about/careers" className="dropdown-item">Careers</Link></li>
+    </ul>
+  </li>
+
+  {/* Services Dropdown */}
+  <li className="header__nav-item dropdown">
+    <button className="header__nav-link dropdown-toggle">Services</button>
+    <ul className="dropdown-menu">
+      <li><Link href="/services/design" className="dropdown-item">Design</Link></li>
+      <li><Link href="/services/development" className="dropdown-item">Development</Link></li>
+      <li><Link href="/services/marketing" className="dropdown-item">Marketing</Link></li>
+    </ul>
+  </li>
+
+  <li>
+    <Link href="/events" className="header__nav-link">Events</Link>
+  </li>
+
+  <li>
+    <Link href="/images" className="header__nav-link">Images</Link>
+  </li>
+
+  <li>
+    <Link href="/donate" className="header__nav-link header__nav-link--highlight">Donate</Link>
+  </li>
+
+  <li>
+    <Link href="/contact" className="header__nav-link">Contact</Link>
+  </li>
+</ul>
         </nav>
         
         <div className="header__spacer"></div>
